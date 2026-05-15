66c453165d0f39ba9899245c7ecec259ad804f8e
Nikhil Ogia <nikhilogia@gmail.com>
2026-05-15 13:18:28 +0530

    fix next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
+
   images: {
-    domains: ['lh3.googleusercontent.com', 'avatars.githubusercontent.com'],
+    domains: [
+      'lh3.googleusercontent.com',
+      'avatars.githubusercontent.com'
+    ],
     unoptimized: true,
   },
 }
